affb831d3a7cd6e7a1794671d4ae863a13fa474d
edgarespnz <20163070@aloe.ulima.edu.pe>
2023-04-16 18:35:58 -0500

    added: auth-functions

---
+++ estudiajoven/src/enviroments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const enviroment ={
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAhp4-7kYwkti3OHjCeQQDmrxZ9rBcqrek",
+    authDomain: "estudia-joven.firebaseapp.com",
+    projectId: "estudia-joven",
+    storageBucket: "estudia-joven.appspot.com",
+    messagingSenderId: "656092966131",
+    appId: "1:656092966131:web:819dcb9cc9310dfa5e9b86"
+  }
+}
